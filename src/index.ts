0cebdc129bdb0495aac726932b2bed782e074c85
systemthinker <robbertoutenaar@gmail.com>
2021-07-05 11:47:57 +0200

    added types and height to map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log('user is', user);
-console.log('company is', company)
+// console.log('user is', user);
+// console.log('company is', company);
+// console.log('maps is', google);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  }
+  
+});
